07129893fae439e480eefa4f670c46f467a2a824
Joe Gilbert <joegilbertdev@outlook.com>
2024-04-07 18:48:03 +0100

    evaluation
    evaluator finds proportion of keywords

--- src/evaluation/simple-evaluation.functions.ts
+++ src/evaluation/simple-evaluation.functions.ts
@@ -11,17 +11,37 @@ async function predictResult({ question }: { question: string }) {
 }
 
 // Define evaluators
-// Issue: returns 0 if missed some phrases, 1 if returns phrases
+// returns 0 if missed some phrases, 1 if returns phrases
 export const mustMention = async ({ run, example }: { run: Run; example?: Example; }): Promise<EvaluationResult> => {
   const mustMention: string[] = example?.outputs?.must_mention ?? [];
   const output = run?.outputs?.output;
   const score = mustMention.every((phrase) => output.includes(phrase)
   );
+
   return {
     key: "must_mention",
     score: score,
   };
 };
+
+// proportion between 0 and 1 of keyword in list present
+export const proportionKeywordsFound = async ({ run, example }: { run: Run; example?: Example; }): Promise<EvaluationResult> => {
+    const keywords: string[] = example?.outputs?.mentions_keywords ?? [];
+    const output = run?.outputs?.output as string;
+    const lowerCasedOutput = output.toLowerCase();
+
+    let keywords_found = 0;
+    keywords.forEach((keyword) => {
+        if (lowerCasedOutput.includes(keyword)) keywords_found += 1;
+    });
+
+    const score = (keywords_found / keywords.length);
+
+    return {
+      key: "mentions_keywords",
+      score: score,
+    };
+  };
 
 // run evaluation
 export const runEvaluation = async () => {
@@ -29,7 +49,7 @@ export const runEvaluation = async () => {
         predictResult,
         datasetName, // The data to predict and grade over
         {
-            evaluationConfig: { customEvaluators: [mustMention] },
+            evaluationConfig: { customEvaluators: [proportionKeywordsFound] },
             projectMetadata: {
                 version: "1.0.0",
                 revision_id: "beta",
--- src/evaluation/simple-evaluation.ts
+++ src/evaluation/simple-evaluation.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import { Client } from "langsmith";
-import { TEST_EVALUATION_INPUT, TEST_EVALUATION_OUTPUTS } from "../ollama/constants.js";
+import { TEST_EVALUATION_INPUT, TEST_EVALUATION_KEYWORDS, TEST_EVALUATION_OUTPUTS } from "../ollama/constants.js";
 import { runEvaluation } from "./simple-evaluation.functions.js";
 
 dotenv.config();
@@ -10,7 +10,6 @@ export const datasetName = "DnD Battle Log: Contains all phrases";
 
 try {
  // Define dataset: these are your test cases
- console.log('Creating new dataset ', datasetName)
 const dataset = await client.createDataset(datasetName, {
     description: "Dungeons and Dragons 3 battle log prompts.",
   });
@@ -20,13 +19,14 @@ const dataset = await client.createDataset(datasetName, {
           {question: TEST_EVALUATION_INPUT},
       ],
       outputs: [
-          {must_mention: TEST_EVALUATION_OUTPUTS},
+          {mentions_keywords: TEST_EVALUATION_KEYWORDS}
       ],
       datasetId: dataset.id,
   });
   
-  console.log('Running evaluations')
-  await runEvaluation()
+  console.log('Creating new dataset ', datasetName);
+  console.log('Running evaluations');
+  await runEvaluation();
 } catch (error: any) {
     console.log('Running evaluations')
     await runEvaluation()
